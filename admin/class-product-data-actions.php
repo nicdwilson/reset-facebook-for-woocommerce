7f548462f6cd93defa7e6c6258a2dc4f6f1975f4
Nic Wilson <3624025+nicdwilson@users.noreply.github.com>
2022-04-05 18:14:07 +1200

    Add reset meta, category argument to delete and reset product actions

--- admin/class-product-data-actions.php
+++ admin/class-product-data-actions.php
@@ -64,6 +64,76 @@ class Product_Data_Actions {
 		$wpnonce = $_REQUEST['_wpnonce'];
 		$this->do_security_checks( $wpnonce );
 
+		echo '<pre>';
+		echo 'Deleting product data...<br>';
+		/**
+		 * A lock is set to prevent two jobs running simultaneously. This is renewed while the loop runs.
+		 * Currently set to 60 seconds
+		 * todo reset to 300
+		 */
+		$is_running = get_transient( 'tfb4wc_joblock' );
+
+		if ( $is_running === '1' ) {
+			die( 'A Facebook products troubleshooting job is running and has not completed or may have stalled. Please wait a few minutes and try again.<br>' . $this->get_backlink() );
+		} else {
+			echo 'Setting lock.<br>';
+			set_transient( 'tfb4wc_joblock', 1, 60 );
+		}
+
+		$offset         = (int) get_option( 'tfb4wc_reset_offset', 0 );
+		$posts_per_page = ( isset( $_POST['posts_per_page'] ) ) ? sanitize_text_field( $_POST['posts_per_page'] ) : '10';
+		$product_cat    = ( isset( $_POST['reset_product_cat'] ) ) ? sanitize_text_field( $_POST['reset_product_cat'] ) : '0';
+		$arguments      = $this->build_query_arguments( $posts_per_page, $product_cat );
+
+		echo 'Found ' . $this->count_products() . ' products on site<br>';
+
+		do {
+
+			$arguments['offset'] = $offset;
+			$product_ids         = get_posts( $arguments );
+
+			if ( ! empty( $product_ids ) ) {
+
+				$integration = facebook_for_woocommerce()->get_integration();
+
+				foreach ( $product_ids as $product_id ) {
+					echo 'Resetting product ID: ' . $product_id . '<br>';
+					$integration->reset_single_product( $product_id );
+				}
+			}
+
+			/**
+			 * Increment the offset
+			 */
+			$offset += $posts_per_page;
+
+			/**
+			 * Keep the job lock current
+			 */
+			echo 'Resetting lock.<br>';
+			set_transient( 'tfb4wc_joblock', 1, 60 );
+
+			/**
+			 * Keep track of how far we made it in case we hit a script timeout
+			 */
+			echo 'Resetting offset<br>';
+			update_option( 'tfb4wc_reset_offset', $offset );
+
+		} while ( count( $product_ids ) == $posts_per_page );
+
+		/**
+		 * Clean up job lock and reset the offset
+		 */
+		echo 'Removing lock.<br>';
+		delete_transient( 'tfb4wc_joblock' );
+		echo 'Deleting offset.<br>';
+		delete_option( 'tfb4wc_reset_offset' );
+
+		/**
+		 * Provide a back link
+		 */
+		echo $this->get_backlink();
+
 	}
 
 	/**
@@ -93,8 +163,8 @@ class Product_Data_Actions {
 		}
 
 		$offset         = (int) get_option( 'tfb4wc_delete_offset', 0 );
-		$posts_per_page = ( isset( $_POST['delete_posts_per_page'] ) ) ? (int) $_POST['delete_posts_per_page'] : 10;
-		$product_cat    = ( isset( $_POST['delete_product_cat'] ) ) ? (int) $_POST['delete_product_cat'] : 0;
+		$posts_per_page = ( isset( $_POST['posts_per_page'] ) ) ? sanitize_text_field( $_POST['posts_per_page'] ) : 10;
+		$product_cat    = ( isset( $_POST['delete_product_cat'] ) ) ? sanitize_text_field( $_POST['delete_product_cat'] ) : 0;
 		$arguments      = $this->build_query_arguments( $posts_per_page, $product_cat );
 
 		echo 'Found ' . $this->count_products() . ' products on site<br>';
--- admin/templates/tab-reset-product-data.php
+++ admin/templates/tab-reset-product-data.php
@@ -8,20 +8,51 @@ if ( ! defined( 'WPINC' ) ) {
 ?>
 
 <p>This will reset the metadata of all products in the catalogue <i>if they were created by Facebook for
-		WooCommerce </i></p>
+        WooCommerce </i></p>
 
 <form action="<?php echo esc_url( admin_url() ); ?>admin-post.php" method="post">
 
-	<input type="hidden" name="action" value="reset_product_data">
+	<?php wp_nonce_field( 'product_data_job' ); ?>
+    <input type="hidden" name="action" value="reset_facebook_product_data">
 
-	<label for="reset_posts_per_page">Batch size:</label>
-	<select id="reset_posts_per_page" name="posts_per_page">
-		<option value="20">10</option>
-		<option value="20">25</option>
-		<option value="20" selected>50</option>
-		<option value="20">100</option>
-		<option value="20">20</option>
-	</select>
+    <table>
+        <tbody>
+        <tr>
+            <td>
+                <label for="reset_posts_per_page">Batch size:</label>
+            </td>
+            <td>
+                <select id="reset_posts_per_page" name="posts_per_page">
+                    <option value="10">10</option>
+                    <option value="25">25</option>
+                    <option value="50" selected>50</option>
+                    <option value="100">100</option>
+                </select>
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+                <label for="reset_product_cat">Category:</label>
+            </td>
+            <td>
+	            <?php
+	            $args = array(
+		            'name'              => 'reset_product_cat',
+		            'value'             => 'term_id',
+		            'taxonomy'          => 'product_cat',
+		            'option_none_value' => 0,
+		            'show_option_none'  => '',
+		            'show_option_all'   => 'All'
+	            );
+	            wp_dropdown_categories( $args );
+	            ?>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+
+
 
 	<?php
 	echo submit_button(
